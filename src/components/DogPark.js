446484850458af0fe2ed53a777499006138d43ae
MukulShivanandTenguria <mukul@preciousinfosystem.com>
2023-02-15 19:49:21 +0530

    Initial comment 2

--- src/components/DogPark.js
+++ src/components/DogPark.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import { Link, useParams } from 'react-router-dom';
 import { spotlist } from '../data'
 import DogParkCard from './DogParkCard'
+import axios from "axios";
+
 const DogPark = () => {
-  console.log(spotlist);
+  const [dogparklist, setDogParkLIst] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/spots/")
+      .then((value) => setDogParkLIst([...value.data]));
+  }, []);
   return (
     <div className="flex flex-wrap gap-4 items-center justify-center m-10">
       {
-        spotlist&&spotlist.map((values)=>{
+        spotlist&&spotlist.map((values,index)=>{
           return(
-            <Link to={`/spots/${values.id}`}>
+            <Link to={`/spots/${index}`}>
             <DogParkCard
+            indexvalue={index}
             value={values}
             />
             </Link>
--- src/components/ParkDetailPage.js
+++ src/components/ParkDetailPage.js
@@ -1,14 +1,64 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-
+import React from "react";
+import { useParams } from "react-router-dom";
+import { spotlist } from "../data";
+import {
+  faRulerCombined,
+  faRulerHorizontal,
+  faStar,
+  faLocationDot,
+  faMapLocationDot,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const ParkDetailPage = () => {
-  const id=useParams()
-  console.log(id);
+  const { id } = useParams();
+  const carddetail = spotlist[id];
+  console.log(carddetail);
   return (
-    <div>
-      
+    <div className="flex justify-center m-10 ">
+      <div className="w-[50%] rounded overflow-hidden shadow-lg">
+        <img
+          className="w-full rounded-md"
+          src="https://media.istockphoto.com/id/1389600745/photo/rottweiler-dog-and-alaskan-malamute-in-the-park.jpg?s=1024x1024&w=is&k=20&c=HTcc1kw4s-i3Nik60S02pSJzmdvAQEwQeuLfS9PwD6I="
+          alt="Sunset in the mountains"
+        />
+        <div className="px-2 py-2">
+          <div className=" text-sm mb-2 font-sans">{carddetail.title}</div>
+          <p className="text-gray-700 text-sm font-serif">
+            {carddetail.description}
+          </p>
+        </div>
+        <div className="flex justify-between">
+          <div className="px-2">
+            <span className="inline-block bg-gray-100 font-serif rounded-full px-3 py-1 text-sm  text-gray-900 mr-2 ">
+              <FontAwesomeIcon icon={faRulerHorizontal} />
+              Fully Fenced
+            </span>
+            <span className="inline-block bg-gray-100 font-serif rounded-full px-3 py-1 text-sm  text-gray-900 mr-2 ">
+              <FontAwesomeIcon icon={faRulerCombined} />
+              &#60; 1 acre
+            </span>
+          </div>
+          <div className="px-3 font-serif">
+            <span>
+              <FontAwesomeIcon color="#f2d00c" icon={faStar} /> 5&#91;16&#93;
+            </span>
+          </div>
+        </div>
+        <div className="flex justify-between ">
+          <div className="px-2 py-1">
+            <span className="inline-block font-serif bg-gray-100 rounded-full px-3 py-1 text-sm  text-gray-900 mr-2 mb-2">
+              &#36;10.00 dog/hour
+            </span>
+          </div>
+          <div className="px-3 py-1">
+            <span className="font-serif text-sm text-gray-900">
+              <FontAwesomeIcon icon={faLocationDot} /> Seattla, WA
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ParkDetailPage
+export default ParkDetailPage;
